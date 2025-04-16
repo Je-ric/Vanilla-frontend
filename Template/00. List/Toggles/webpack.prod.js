ace086780c0b679263a13f21b0cf11e28fd42271
Jeric Dela Cruz <delacruz.jeric.j@gmail.com>
2025-04-16 22:00:55 +0800

    Removing Unwanted Files - P1

--- Template/00. List/Toggles/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const dev = require('./webpack.dev.js');
-
-module.exports = merge(dev, {
-    mode: 'production',
-    devtool: 'source-map',
-});

318fc5d73f326c9d01d766b0c1bfd298d579d010
Je-ric <delacruz.jeric.j@gmail.com>
2025-04-29 20:42:20 +0800

    Deleting unused files (again)

--- Template/10. Button-Border-Icon-Loader/3dot (Icon)/script.js
+++
@@ -1,9 +0,0 @@
-const expandBtn = document.querySelector("[data-position ='expand_btn']");
-const buttons = Array.from(document.querySelectorAll('[data-position]'));
-
-expandBtn.addEventListener('click', () => {
-  buttons.map(btn => {
-  const pos = btn.dataset['position'];
-  btn.classList.toggle(pos);
-})
-})
--- Template/10. Button-Border-Icon-Loader/Password (Button)/script.js
+++
@@ -1,9 +0,0 @@
-const svg = document.querySelector("svg");
-const input = document.querySelector("input");
-
-svg.addEventListener("click", function(){
-	this.classList.toggle("close");
-	setTimeout(() => {
-		input.type = input.type === "password" ? "text" : "password";
-	}, 125);
-});
